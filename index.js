a1f8ab2da55d217ca68cd8b593518265bb601d3f
KelsieHargreaves <kelsie.hargreaves@yahoo.com>
2026-01-01 23:21:18 -0600

    Initial commit

--- index.js
+++ index.js
@@ -2,6 +2,50 @@ let isModalOpen = false;
 let contrastToggle = false;
 const scaleFactor = 1/20;
 
+const flipbook = document.querySelector('.flipbook');
+const pages = document.querySelectorAll('.flipbook .page');
+
+let currentPage = 0;
+let interval = null;
+
+function flipPage() {
+  if (currentPage >= pages.length) {
+    pages.forEach(page => page.classList.remove('flipped'));
+    currentPage = 0;
+    return;
+  }
+
+  pages[currentPage].classList.add('flipped');
+  currentPage++;
+}
+
+
+function startFlip() {
+  if (!interval) interval = setInterval(flipPage, 1500);
+}
+
+function stopFlip() {
+  clearInterval(interval);
+  interval = null;
+}
+
+
+flipbook.addEventListener('mouseenter', stopFlip);
+flipbook.addEventListener('mouseleave', startFlip);
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        startFlip();
+        observer.unobserve(flipbook);
+      }
+    });
+  }, {
+    threshold: 0.5
+  });
+
+  observer.observe(flipbook);
+
 function moveBackground(event) {
     const shapes = document.querySelectorAll(".shape");
     const x = event.clientX * scaleFactor;
@@ -32,7 +76,7 @@ function contact(event) {
 
     emailjs
         .sendForm(
-            'service_ib2pwf3',
+            'service_3nyjr32',
             'template_kmpjhuc',
             event.target,
             'RjPmjINvQvyghwuH6'
